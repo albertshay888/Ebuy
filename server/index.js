a6ab9cebac5436165eaf8eb635359597a0236dd3
Albert Shay <albertshay888@gmail.com>
2021-10-18 14:06:04 -0700

    added deployment middleware

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 import postRoutes from './routes/posts.js';
 import userRouter from './routes/user.js';
 dotenv.config();
@@ -10,7 +11,10 @@ const app = express();
 
 app.use(cors());
 //localhost:5000/posts
-
+app.use(express.static(`client/build`));
+app.get(`*`, (req, res) => {
+  res.sendFile(path.resolve(__dirname, `client`, `build`, `index.html`));
+});
 //general middleware setup
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
