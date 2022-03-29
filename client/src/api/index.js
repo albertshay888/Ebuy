8163e68e4344f88ba7f47cdbdf3177e8eaf520d3
Albert Shay <20329123+albertshay888@users.noreply.github.com>
2022-03-28 20:05:34 -0700

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: `https://silkroadsss.herokuapp.com/`,
+  baseURL: 'http://silkroadsss.herokuapp.com'
 });
 
 API.interceptors.request.use((req) => {
